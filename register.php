5c8d8333e9ecf14a041f754e7c65f6405fa1bdc2
Suri <19am1a0553@svrec.ac.in>
2022-05-17 22:14:30 +0530

    adding php

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$dob = $_POST['dob'];
+
+
+
+$conn = new mysqli('localhost','root','','forms');
+if($conn->connect_error)
+{
+    die("Connection failed : ".$conn->connect_error);
+}
+else 
+{
+    $stmt = $conn->prepare("insert into register(fname,lname,mail,password,dob) values(?,?,?,?,?)");
+    $stmt->bind_param("sssss",$fname,$lname,$mail,$password,$dob);
+    $stmt->execute();
+    echo "<h1>Registration Successful.......</h1>";
+    $stmt->close();
+    $conn->close();
+}
+
+?>
